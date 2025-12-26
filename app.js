2bdd559cb560243b1cfd564beb2b7ba3aee0e6e8
Amna Azhar <mehmoodazhar3729@gmail.com>
2025-12-26 17:00:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+// PDF 1 CHP:Alerts//
+
+//QUESTION 01//
+
+
+//alert("welcome to my website");
+
+
+// question 02//
+
+//alert("Error! please enter a valid password.");
+
+// question 03 //
+
+//alert("welcome to JS land\nHappy coding!");
+
+//question 04 //
+
+//alert("welcome to JS land");
+//alert("Happy coding!");
+
+//question 05 //
+ 
+//console.log("Hello...I can run JS through my web browser's console");
+
+//question 06 skip kr dia ap ne khud bola tha..⊙⁠﹏⁠⊙//
+
+//question 07 Skip //
+
+// PDF 2  CHP: VARIABLES FOR STRINGS //
+
+// QUESTION 01//
+
+//var username;
+
+// QUESTION 02//
+
+//var myname="Amna Azhar";
+
+//QUESTION 03//
+
+//var message;
+//message="Hello World";
+//alert(message);
+
+//QUESTION 04//
+
+//var studentName="Amna";
+//var studentAge="15";
+//var course="Information Technology";
+
+//alert(studentName);
+//alert(studentAge);
+//alert(course);
+
+//QUESTION 05//
+
+//var food = "pizza\npizz\npiz\npi\np";
+//alert(food);
+
+//QUESTION 06//
+
+//var email = "amnaazhar481@gmail.com";
+//alert("my email address is "  +  email);
+
+//QUESTION 07 //
+
+//var book = " A smarter way to learn javaScript";
+//alert("I am trying to learn from the book" + book);
+
+//QUESTION 08//
+
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//QUESTION 9//
+
+//var design =
+//"▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(design);
+//document.write(design);
+
+//PDF 3 CHP: VARIABLES FOR NUMBERS
+//QUESTION 1 //
+
+//var age = 15;
+//alert("I am " + age +" year old");
+
+//QUESTION 02//
+
+//var visitCount = 3;
+//document.write("You have visited this site " + visitCount + " times.<br>");
+
+//QUESTION 03//
+
+//var birthYear =2010;
+//document.write("My birthYear is " + birthYear + "<br>");
+//document.write("Data type of my declared variable is number <br>");
+
+//QUESTION 04//
+
+//var visitorName = "Amna"
+//var productTitle = "cargo pant"
+//var quantity = 3
+//document.write(visitorName  +  " ordered " + quantity  + " "  + productTitle  +  "(s) on XYZ Clothing store.");
+
+// PDF 4 CHP:VARIABLE NAMES: LEGAL & ILLEGAL//
+//QUESTION 01//
+
+//var firstName, rollNo, country;
+
+//QUESTION 02 Legal variable Names//
+
+//var studentName;
+//var $total;
+//var finalScore;
+//var userId4;
+//var user_name;
+
+//Illegal variable Names //
+
+//var 2start;
+//var full name;
+// var #point;
+// var let;
+//var my-name;
+
+// QUESTION 03//
+
+// a) document.write("<h1>Rules for naming JS variables</h1>");
+
+// b) document.write("Variable names can only contain letter, digits, $ or _. Example : $user_2 <br><br>");
+
+// c) document.write(" Variables must begin with a letter,$ or _. Example: $name,_name or name<br><br>");
+
+// d) document.write("Variable names are case sensitive");
+
+//e) document.write(" Variable names should not be JS keywords");
+
+ // (⁠.❛⁠⁠ᴗ⁠⁠❛⁠.)**END**(⁠.❛⁠⁠ᴗ⁠⁠❛⁠.)(⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)//
